8d3e09876fce8d38cadfbac667f15a1f68c1866f
Дмитрий Александров <mapoch2002@yandex.ru>
2022-02-24 12:33:24 +0400

    q

---
+++ src/test/java/ru/ulstu/is/shapp/ShappApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ulstu.is.shapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
